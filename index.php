0fe750497255e2cd95ca76247bbd7ccc8a70a430
robertoDorado <robertodorado7@gmail.com>
2023-08-25 16:21:18 -0300

    log de erros incluso no framework

--- index.php
+++ index.php
@@ -6,6 +6,19 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Source\Core\MyRouter;
 
+date_default_timezone_set("America/Sao_Paulo");
+
+$path = "./Logs/error.log";
+
+if (!file_exists($path)) {
+    mkdir("Logs");
+    file_put_contents($path, '');
+}
+
+error_reporting(E_ALL & (~E_NOTICE | ~E_USER_NOTICE));
+ini_set('error_log', $path);
+ini_set('log_errors', true);
+
 $route = new MyRouter(url(), ":");
 
 /**
