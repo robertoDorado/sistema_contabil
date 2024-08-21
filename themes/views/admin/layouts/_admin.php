f1788f8dd557784daa45afb92e61181a28c00e8b
robertoDorado <robertodorado7@gmail.com>
2024-08-21 18:32:46 -0300

    emissão de nota fiscal em desenvolvimento

--- themes/views/admin/layouts/_admin.php
+++ themes/views/admin/layouts/_admin.php
@@ -54,6 +54,22 @@
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <li class="nav-item">
                         <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-money-check"></i>
+                            <p>
+                                Menu fiscal
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <p>Emissão de nota fiscal</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-balance-scale"></i>
                             <p>
                                 Balanço patrimonial
