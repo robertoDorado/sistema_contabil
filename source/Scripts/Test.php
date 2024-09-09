4e1d70af894d2e3ea81dcac86cb5192dd2eb8aa7
robertoDorado <robertodorado7@gmail.com>
2024-09-09 20:46:30 -0300

    atualização na checkagem de usuários gratuitos

--- source/Scripts/Test.php
+++ source/Scripts/Test.php
@@ -1,2 +1,8 @@
 <?php
-echo (208.75*(1-(3.99/100)))-0.39 . PHP_EOL;
+$subscriptionPriceYear = 156.75 * 12;
+$subscriptionPriceMonth = 208.75;
+echo "Plano mensal: R$ " . number_format($subscriptionPriceMonth, 2, ",", ".") . PHP_EOL;
+echo "Plano anual: R$ " . number_format($subscriptionPriceYear, 2, ",", ".") . PHP_EOL;
+echo "----------------------------" . PHP_EOL;
+echo "Lucro mensal: R$ " . number_format(($subscriptionPriceMonth*(1-(3.99/100)))-0.39, 2, ",", ".") . PHP_EOL;
+echo "Lucro anual: R$ " . number_format(($subscriptionPriceYear*(1-(3.99/100)))-0.39, 2, ",", ".") . PHP_EOL;
