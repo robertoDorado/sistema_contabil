05d259d992496efc63be195636141b952b4f6f21
robertoDorado <robertodorado7@gmail.com>
2025-03-28 20:57:39 -0300

    ajuste das credenciais da stripe para produção

--- themes/views/assets/js/customer-subscribe-form.js
+++ themes/views/assets/js/customer-subscribe-form.js
@@ -85,7 +85,7 @@ if (window.location.pathname == "/customer/subscribe") {
             .replace(/(-\d{4})\d+?$/, "$1")
     })
 
-    const stripe = Stripe(stripePublicKeys().test, {
+    const stripe = Stripe(stripePublicKeys().live, {
         locale: "pt-BR"
     })
 
