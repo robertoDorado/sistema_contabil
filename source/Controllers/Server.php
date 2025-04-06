2f43327715ac8993b4d68faaf8ed6458e65f5090
robertoDorado <robertodorado7@gmail.com>
2025-04-06 14:41:01 -0300

    otimização na atualização dos dados de assinatura pelo webhook

--- source/Controllers/Server.php
+++ source/Controllers/Server.php
@@ -27,6 +27,40 @@ class Server extends Controller
         parent::__construct();
     }
 
+    private function updateSubscriptionData(Event $event): array
+    {
+        $id = $event->data->object->lines->data[0]->parent->subscription_item_details->subscription;
+        $subscription = new Subscription();
+
+        $subscription->subscription_id = $id;
+        $subscriptionData = $subscription->findSubsCriptionBySubscriptionId([]);
+
+        if (empty($subscriptionData)) {
+            throw new Exception($subscription->message->json() . json_encode(["subscription_id" => $id]));
+        }
+
+        $dateTimePeriodStart = new DateTime();
+        $dateTimePeriodStart->setTimestamp($event->data->object->lines->data[0]->period->start);
+
+        $dateTimePeriodEnd = new DateTime();
+        $dateTimePeriodEnd->setTimestamp($event->data->object->lines->data[0]->period->end);
+        $priceValue = formatStripePriceInFloatValue(true, $event->data->object->lines->data[0]->amount);
+
+        $subscription = new Subscription();
+        $response = $subscription->updateSubscriptionBySubscriptionId([
+            "subscription_id" => $id,
+            "price_value" => $priceValue,
+            "subscription_type" => checkSubscriptionType()[$priceValue] ?? "basic",
+            "charge_id" => $event->data->object->charge,
+            "product_description" => $event->data->object->lines->data[0]->description,
+            "updated_at" => $dateTimePeriodStart->format("Y-m-d"),
+            "period_start" => $dateTimePeriodStart->format("Y-m-d"),
+            "period_end" => $dateTimePeriodEnd->format("Y-m-d"),
+            "status" => "active"
+        ]);
+        return empty($response) ? [] : ["subscriptionData" => $subscription, "id" => $id];
+    }
+
     public function webhookUpdateSubscription()
     {
         $payload = @file_get_contents("php://input");
@@ -40,38 +74,9 @@ class Server extends Controller
 
         $validateStripeEvent = [
             "invoice.finalized" => function (Event $event) {
-                $id = $event->data->object->lines->data[0]->parent->subscription_item_details->subscription;
-                $subscription = new Subscription();
-
-                $subscription->subscription_id = $id;
-                $subscriptionData = $subscription->findSubsCriptionBySubscriptionId([]);
-
-                if (empty($subscriptionData)) {
-                    throw new Exception($subscription->message->json() . json_encode(["subscription_id" => $id]));
-                }
-
-                $dateTimePeriodStart = new DateTime();
-                $dateTimePeriodStart->setTimestamp($event->data->object->lines->data[0]->period->start);
-
-                $dateTimePeriodEnd = new DateTime();
-                $dateTimePeriodEnd->setTimestamp($event->data->object->lines->data[0]->period->end);
-                $priceValue = formatStripePriceInFloatValue(true, $event->data->object->lines->data[0]->amount);
-
-                $subscription = new Subscription();
-                $response = $subscription->updateSubscriptionBySubscriptionId([
-                    "subscription_id" => $id,
-                    "price_value" => $priceValue,
-                    "subscription_type" => checkSubscriptionType()[$priceValue] ?? "basic",
-                    "charge_id" => $event->data->object->charge,
-                    "product_description" => $event->data->object->lines->data[0]->description,
-                    "updated_at" => $dateTimePeriodStart->format("Y-m-d"),
-                    "period_start" => $dateTimePeriodStart->format("Y-m-d"),
-                    "period_end" => $dateTimePeriodEnd->format("Y-m-d"),
-                    "status" => "active"
-                ]);
-
+                $response = $this->updateSubscriptionData($event);
                 if (empty($response)) {
-                    throw new Exception($subscription->message->json() . json_encode(["subscription_id" => $id]));
+                    throw new Exception($response["subscriptionData"]->message->json() . json_encode(["subscription_id" => $response["id"]]));
                 }
             },
             "customer.subscription.deleted" => function (Event $event) {
@@ -105,55 +110,10 @@ class Server extends Controller
                 }
             },
 
-            "invoice.payment_succeeded" => function(Event $event) {
-                $id = $event->data->object->charge;
-                $subscription = new Subscription();
-                
-                $subscription->charge_id = $id;
-                $subscriptionData = $subscription->findSubscriptionByChargeId(["customer_id"]);
-
-                if (empty($subscriptionData)) {
-                    throw new Exception("assinatura não encontrada");
-                }
-
-                $customer = new Customer();
-                $customer->setId($subscriptionData->customer_id);
-                $response = $customer->updateCustomerById([
-                    "id" => $subscriptionData->customer_id,
-                    "deleted" => 0
-                ]);
-
+            "invoice.payment_succeeded" => function (Event $event) {
+                $response = $this->updateSubscriptionData($event);
                 if (empty($response)) {
-                    throw new Exception($customer->message->json() . json_encode(["subscription_id" => $id]));
-                }
-
-                $user = new User();
-                $response = $user->updateUserByCustomerId([
-                    "id_customer" => $customer,
-                    "deleted" => 0
-                ]);
-
-                if (empty($response)) {
-                    throw new Exception($user->message->json() . json_encode(["subscription_id" => $id]));
-                }
-
-                $dateTimePeriodStart = new DateTime();
-                $dateTimePeriodStart->setTimestamp($event->data->object->lines->data[0]->period->start);
-
-                $dateTimePeriodEnd = new DateTime();
-                $dateTimePeriodEnd->setTimestamp($event->data->object->lines->data[0]->period->end);
-
-                $subscription = new Subscription();
-                $response = $subscription->updateSubscriptionByChargeId([
-                    "charge_id" => $id,
-                    "updated_at" => $dateTimePeriodStart->format("Y-m-d"),
-                    "period_start" => $dateTimePeriodStart->format("Y-m-d"),
-                    "period_end" => $dateTimePeriodEnd->format("Y-m-d"),
-                    "status" => "active"
-                ]);
-
-                if (empty($response)) {
-                    throw new Exception("não foi possível atualizar a data de renovação da fatura");
+                    throw new Exception($response["subscriptionData"]->message->json() . json_encode(["subscription_id" => $response["id"]]));
                 }
             }
         ];
