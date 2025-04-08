2018516a98c58e42396e065e076e7f9a3beca799
robertoDorado <robertodorado7@gmail.com>
2025-04-08 16:27:40 -0300

    carregamento visível para o usuário

--- themes/views/assets/js/006-display-endpoints.js
+++ themes/views/assets/js/006-display-endpoints.js
@@ -1,19 +1,29 @@
 const endpointsElement = document.querySelector("[endpoints]")
 if (endpointsElement) {
     let endpoints = JSON.parse(endpointsElement.getAttribute("endpoints"))
-    
+
     if (endpoints.length > 0) {
         let sidebarMenu = Array.from(document.querySelector("[sidebarmenu]").firstElementChild.children)
         sidebarMenu = sidebarMenu.filter((element) => element.querySelectorAll(".nav-item"))
         sidebarMenu.forEach((element) => {
             let links = Array.from(element.querySelectorAll(".nav-link"))
             links = links.filter((link) => !link.href.match(/\w#$/g))
-            
+
+            if (links.length > 0) {
+                links.forEach(function (linkElement) {
+                    linkElement.addEventListener("click", function (event) {
+                        event.preventDefault()
+                        modal.style.display = "flex"
+                        window.location.href = linkElement.href
+                    })
+                })
+            }
+
             links = links.filter((link) => {
                 const url = new URL(link.href)
                 return endpoints.includes(url.pathname)
             })
-            
+
             if (links.length > 0) {
                 element.classList.add("menu-open")
                 element.firstElementChild.classList.add("active")
@@ -21,7 +31,7 @@ if (endpointsElement) {
                 links.forEach((link) => {
                     link.closest("li").classList.add("menu-open")
                     link.classList.add("active")
-    
+
                     link.closest("li").closest("ul").closest("li").classList.add("menu-open")
                     link.closest("li").closest("ul").closest("li").firstElementChild.classList.add("active")
                 })
